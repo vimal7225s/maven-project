e9e6110720a3c79235b12bd0c961cd7c94f4a19b
vimalraj subramanian <vimalraj.subramanian@lashgroup.com>
2018-11-14 11:59:08 -0600

    Modified Unit Test Case

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,6 +1,7 @@
 package com.mulesoft.training;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 public class HelloMavenTest extends FunctionalTestCase {
     @Test
@@ -11,5 +12,12 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
 }
